bde55bd240e1fb32540a735144c8e4db4bd0df21
kingfariz <kungfupanda22@gmail.com>
2024-10-07 19:33:52 +0700

    minor clean code

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -14,7 +14,7 @@ export default function Home() {
         <Header />
         <Origin />
         <Roadmap />
-        <TrustedBy />
+        {/* <TrustedBy /> */}
         <Faq />
         <Footer />
       </main>
