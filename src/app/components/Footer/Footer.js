21f2129e0635a26f3b7bf55bf5ccb929745ebccd
kingfariz <kungfupanda22@gmail.com>
2024-12-15 23:33:56 +0700

    refactor: Refactor react js to next js

---
+++ src/app/components/Footer/Footer.js
@@ -0,0 +1,24 @@
+import React from "react";
+import "./Footer.css";
+import { Container } from "react-bootstrap";
+
+export default function Footer() {
+  return (
+    <footer id="footer">
+      <Container>
+        <div className="content">
+          <div className="logo">
+            <img
+              src="/assets/footer_logo.png"
+              alt="ISEEKAIGO"
+              className="h-16 md:h-24 w-auto"
+            />
+          </div>
+          <p className="footer-text">
+            ISEEKAIGO Project. All Rights Reserved 2024.
+          </p>
+        </div>
+      </Container>
+    </footer>
+  );
+}
